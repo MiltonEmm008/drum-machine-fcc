9f74136a3b2995ea6e02da7e640f8f0826ac04ef
MiltonEmm008 <merp2067@gmail.com>
2025-07-05 13:13:05 -0700

    fix audio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  const base = isProduction ? "/drum-machine-fcc/" : "/";
+  
+  return {
+    plugins: [react()],
+    base,
+  };
 });
