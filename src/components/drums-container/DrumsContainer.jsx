3df29f6fc86e32a589c4def63649161f7c5c846d
MiltonEmm008 <merp2067@gmail.com>
2025-07-05 13:16:43 -0700

    fix audio routes

--- src/components/drums-container/DrumsContainer.jsx
+++ src/components/drums-container/DrumsContainer.jsx
@@ -4,50 +4,61 @@ import { ActionContext } from "../../context/ActionContext";
 import styles from "../drum-pad/drumpad.module.scss";
 import drum_style from "./drumcontainer.module.scss";
 
+// Import audio files
+import heater1 from "../../assets/audio/Heater-1.mp3";
+import heater2 from "../../assets/audio/Heater-2.mp3";
+import heater3 from "../../assets/audio/Heater-3.mp3";
+import heater4 from "../../assets/audio/Heater-4_1.mp3";
+import heater6 from "../../assets/audio/Heater-6.mp3";
+import dscOh from "../../assets/audio/Dsc_Oh.mp3";
+import kickNHat from "../../assets/audio/Kick_n_Hat.mp3";
+import rp4Kick from "../../assets/audio/RP4_KICK_1.mp3";
+import cevH2 from "../../assets/audio/Cev_H2.mp3";
+
 const drumsData = [
   {
     keyword: "Q",
-    audioSrc: "/audio/Heater-1.mp3",
+    audioSrc: heater1,
     audioDesc: "Heater 1",
   },
   {
     keyword: "W",
-    audioSrc: "/audio/Heater-2.mp3",
+    audioSrc: heater2,
     audioDesc: "Heater 2",
   },
   {
     keyword: "E",
-    audioSrc: "/audio/Heater-3.mp3",
+    audioSrc: heater3,
     audioDesc: "Heater 3",
   },
   {
     keyword: "A",
-    audioSrc: "/audio/Heater-4_1.mp3",
+    audioSrc: heater4,
     audioDesc: "Heater 4",
   },
   {
     keyword: "S",
-    audioSrc: "/audio/Heater-6.mp3",
+    audioSrc: heater6,
     audioDesc: "Clap",
   },
   {
     keyword: "D",
-    audioSrc: "/audio/Dsc_Oh.mp3",
+    audioSrc: dscOh,
     audioDesc: "Open HHzz",
   },
   {
     keyword: "Z",
-    audioSrc: "/audio/Kick_n_Hat.mp3",
+    audioSrc: kickNHat,
     audioDesc: "Kick n Hat",
   },
   {
     keyword: "X",
-    audioSrc: "/audio/RP4_KICK_1.mp3",
+    audioSrc: rp4Kick,
     audioDesc: "Kick",
   },
   {
     keyword: "C",
-    audioSrc: "/audio/Cev_H2.mp3",
+    audioSrc: cevH2,
     audioDesc: "Closed HH",
   },
 ];
